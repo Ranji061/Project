92db332ec241620f111bacb54ae1c1c59c1336cf
Ranji061 <140324114+Ranji061@users.noreply.github.com>
2023-07-23 19:10:45 +0530

    Add files via upload

---
+++ BookStore.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BookStore.aspx.cs" Inherits="BookStore.BookStore" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Book Store Details</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           <h1><b>Book Store Details</b></h1>
+         
+            <asp:Label runat="server"><b>Book Name   :</b>&nbsp&nbsp</asp:Label>
+            <asp:TextBox runat="server" id="BookName" placeholder="Enter Book Name" OnTextChanged="BookName_TextChanged" style="margin-left: 21px" Width="311px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label runat="server"><b>Author Name  :</b>&nbsp&nbsp</asp:Label>
+            <asp:TextBox runat="server" id="Authorid" placeholder="Enter Author Name" style="margin-left: 7px" Width="304px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label runat="server"><b>No of Books :</b>&nbsp&nbsp</asp:Label>
+            <asp:TextBox runat="server" id="NoBoks" style="margin-left: 15px" Width="221px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button runat="server" style="margin-left: 101px;" Text="Insert" ID="Submit" OnClick="Add_onclick" BorderColor="Red" BorderStyle="Double" Font-Bold="True" Font-Size="11pt" Height="39px" Width="125px" BackColor="#CC3300" ForeColor="Black"/>
+            <asp:Button ID="btnclear" runat="server" BackColor="#CC3300" BorderColor="Red" Height="38px" style="margin-left: 44px" Text="Clear" Width="149px" OnClick="btnclear_Click" />
+            <br />
+            <br />
+            
+
+
+            <asp:DataGrid runat="server" ID="BkStore" AutoGenerateColumns="false" Width="732px">
+                  <Columns>  
+                        <asp:BoundColumn HeaderText="BookName" DataField="BookName"> </asp:BoundColumn>  
+                        <asp:BoundColumn HeaderText="AuthorName" DataField="AuthorName"> </asp:BoundColumn>  
+                        <asp:BoundColumn HeaderText="NoofBooks" DataField="NoofBooks"> </asp:BoundColumn>  
+                       <asp:TemplateColumn >
+                          <ItemTemplate>
+                              <asp:LinkButton ID="btnUpdate" Text="Update" runat="server" CommandArgument='<% # Eval("Bkid") %>' OnClick="btnUpdate_Click1"></asp:LinkButton>
+                          </ItemTemplate>
+                       </asp:TemplateColumn>  
+                         <asp:TemplateColumn >
+                          <ItemTemplate>
+                              <asp:LinkButton ID="BtnDelete" Text="Delete" runat="server" CommandArgument='<% # Eval("Bkid") %>' OnClick="BtnDelete_Click"></asp:LinkButton>
+                          </ItemTemplate>
+                       </asp:TemplateColumn> 
+                    </Columns> 
+                 <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />  
+                    <SelectedItemStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />  
+                    <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" Mode="NumericPages" />  
+                    <AlternatingItemStyle BackColor="White" />  
+                    <ItemStyle BackColor="#FFFBD6" ForeColor="#333333" />  
+                    <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" /> </asp:DataGrid> <br /> <br /> 
+
+           
+
+ 
+
+
+            
+
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ BookStore.aspx.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Reflection.Emit;
+using System.Text;
+using System.IO;
+using System.Web.Services.Description;
+using System.Windows.Controls;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Drawing;
+using System.Windows;
+
+namespace BookStore
+{
+    public partial class BookStore : System.Web.UI.Page
+    {
+        public SqlConnection cnn;
+        public SqlCommand cmd;
+        StringBuilder StrSb = new StringBuilder();
+        SqlDataAdapter da = new SqlDataAdapter();
+        public String StrQuery = "";
+        public DataTable Dt;
+        public string  Strmessage="";
+        private LinkButton sender;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                GetConnection();
+                if (!Page.IsPostBack)
+                {
+                    BindData();
+
+                }
+            }catch(Exception ex)
+            {
+                ErrorLog("Page_Load", ex.ToString());
+            }
+
+        }
+        public void GetConnection()
+        {
+            try
+            {
+                String StrConnection = "";
+                StrConnection = "Data source=LAPTOP-BDL7RB39;Initial Catalog=BookStore;user id=sa;password=swspg";
+                cnn = new SqlConnection(StrConnection);
+                cnn.Open();
+
+            }
+            catch(Exception ex)
+            {
+                ErrorLog("GetConnection", ex.ToString());
+            }
+        }
+        protected void Add_onclick(object sender, EventArgs e)
+        {
+            try
+            {
+                GetBooksDetails();
+               
+            }
+            catch(Exception ex)
+            {
+                ErrorLog("Add_onclick", ex.ToString());
+            }
+
+        }
+        public void BindData()
+        {
+            try
+            {
+                StrQuery = "select Bkid,BookName,AuthorName,NoofBooks from BookStore..mstbookstore";
+                cmd = new SqlCommand(StrQuery, cnn);
+                da = new SqlDataAdapter(cmd);
+                Dt = new DataTable();
+                da.Fill(Dt);
+                if (Dt.Rows.Count > 0)
+ {
+                    BkStore.DataSource = Dt;
+                    BkStore.DataBind();
+                }
+                else
+                {
+                    BkStore.DataSource = null;
+                }
+               
+            }
+            catch(Exception ex)
+            {
+                ErrorLog("BindData", ex.ToString());
+            }
+
+            
+        }
+        
+        public void GetBooksDetails()
+        {
+            try
+            {
+                string Booknameid = Request.Form["BookName"];
+                string authorname = Request.Form["Authorid"];
+                string Noofbooks = Request.Form["NoBoks"];
+
+                
+                StrQuery = "";
+                StrQuery = "select * from BookStore..mstbookstore where BookName='" + Booknameid.ToString()+ "' and AuthorName='"+ authorname.ToString ()+"'";
+                cmd = new SqlCommand(StrQuery, cnn);
+                da = new SqlDataAdapter(cmd);
+                Dt = new DataTable();
+                da.Fill(Dt);
+
+                if (Dt.Rows.Count > 0)
+                {
+                    if (Submit.Text != "Delete")
+                    {
+                        StrQuery = "";
+                        StrQuery = "update BookStore..mstbookstore set NoofBooks=" + Noofbooks.ToString() + " where BookName='" + Booknameid.ToString() + "' and AuthorName='" + authorname.ToString() + "' and Bkid="+Dt.Rows [0]["Bkid"].ToString();
+                        cmd = new SqlCommand(StrQuery, cnn);
+                        cmd.ExecuteNonQuery();
+                        Strmessage = "Updated BookStock Successfully";
+                        ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + Strmessage + "');", true);
+                        BindData();
+                        BookName.Text = "";
+                        Authorid.Text = "";
+                        NoBoks.Text = "";
+                    }
+                    else
+                    {
+                        
+
+                        StrQuery = "";
+                        StrQuery = "delete BookStore..mstbookstore where Bkid = "+Dt.Rows [0]["Bkid"].ToString();;
+                        cmd = new SqlCommand(StrQuery, cnn);
+                        cmd.ExecuteNonQuery();
+                        Strmessage = "Successfully Deleted";
+                        ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + Strmessage + "');", true);
+                        BindData();
+                        BookName.Text = "";
+                        Authorid.Text = "";
+                        NoBoks.Text = "";
+
+                    }
+                }
+                else
+                {
+                    StrQuery = "";
+                    StrQuery = "Insert into BookStore..mstbookstore values('" + Booknameid.ToString() + "','" + authorname.ToString() + "'," + Noofbooks.ToString() + ",getdate())";
+                    cmd = new SqlCommand(StrQuery, cnn);
+                    cmd.ExecuteNonQuery();
+                    Strmessage = "Successfully Inserted BooK details";
+                    ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + Strmessage + "');", true);
+                    BindData();
+                    BookName.Text = "";
+                    Authorid.Text = "";
+                    NoBoks.Text = "";
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                ErrorLog("GetBooksDetails", ex.ToString());
+            }
+        }
+       
+
+        protected void BtnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Submit.Text = "Delete";
+                int Book_id = Convert.ToInt32((sender as LinkButton).CommandArgument.ToString());
+                StrQuery = "";
+                StrQuery = "select * from BookStore..mstbookstore where Bkid=" + Book_id.ToString();
+                cmd = new SqlCommand(StrQuery, cnn);
+                da = new SqlDataAdapter(cmd);
+                Dt = new DataTable();
+                da.Fill(Dt);
+
+                if (Dt.Rows.Count > 0)
+                {
+                    BookName.Text = Dt.Rows[0]["BookName"].ToString();
+                    Authorid.Text = Dt.Rows[0]["AuthorName"].ToString();
+                    NoBoks.Text = Dt.Rows[0]["NoofBooks"].ToString();
+ 
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ErrorLog("BtnDelete_Click", ex.ToString());
+            }
+
+        }
+
+        protected void BookName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpdate_Click1(object sender, EventArgs e)
+        {
+            try
+            {
+                Submit.Text = "Update";
+                int Book_id = Convert.ToInt32((sender as LinkButton).CommandArgument.ToString());
+                StrQuery = "";
+                StrQuery = "select * from BookStore..mstbookstore where BkId=" + Book_id.ToString();
+                cmd = new SqlCommand(StrQuery, cnn);
+                da = new SqlDataAdapter(cmd);
+                Dt = new DataTable();
+                da.Fill(Dt);
+
+                if (Dt.Rows.Count > 0)
+                {
+                    BookName.Text = Dt.Rows[0]["BookName"].ToString();
+                    Authorid.Text = Dt.Rows[0]["AuthorName"].ToString();
+                    NoBoks.Text = Dt.Rows[0]["NoofBooks"].ToString();
+                }
+
+
+                }
+            catch (Exception ex)
+            {
+                ErrorLog("btnUpdate_Click1", ex.ToString());
+            }
+        }
+
+        protected void btnclear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BookName.Text = "";
+                Authorid.Text = "";
+                NoBoks.Text = "";
+            }
+            catch(Exception ex)
+            {
+                ErrorLog("btnclear_Click", ex.ToString());
+            }
+        }
+        private void ErrorLog(string strmsg,string strerror)
+        {
+            try
+            {
+                
+                string strlog= AppDomain.CurrentDomain.BaseDirectory + "/bin";
+
+                if (!Directory.Exists(strlog + "/Log"))
+                {
+                    Directory.CreateDirectory(strlog + "/Log");
+                }
+                string filename = strlog + "/Log" + "-" + DateTime.Now.ToString();
+                File.CreateText(filename);
+                File.WriteAllText(strlog, strmsg);
+            
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
